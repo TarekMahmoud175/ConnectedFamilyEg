ba08f0a778b87d698c6d2f2353c92f12f68fce92
TarekMahmoud175 <tarek64@live.com>
2020-07-01 19:36:21 +0200

    created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UserInfoComponent } from './profile/user-info/user-info.component';
+import { UserEventsComponent } from './profile/user-events/user-events.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProfileComponent,
+    UserInfoComponent,
+    UserEventsComponent
   ],
   imports: [
     BrowserModule,
